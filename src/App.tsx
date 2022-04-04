81015f25383a7bebc8f0949e65ed4b03efa984b4
Julian Scharf <julian.scharf@tshirtventures.com.au>
2022-04-04 20:59:44 +1000

    nice sections

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ function App() {
           </header>
           <div className="panel-controls">
             <div className="panel-section">
+              <h2 className="panel-group-label label">Mixer</h2>
               <div className="control-group">
                 <div className="control-wrapper">
                   <Knob
@@ -26,7 +27,7 @@ function App() {
                     color={true}
                     onChange={(value) => console.log(value)}
                   />
-                  <p className="control-label label">Filter</p>
+                  <p className="control-label label">VCO1</p>
                 </div>
               </div>
               <div className="control-group">
@@ -40,7 +41,38 @@ function App() {
                     color={true}
                     onChange={(value) => console.log(value)}
                   />
-                  <p className="control-label label">Filter</p>
+                  <p className="control-label label">VCO2</p>
+                </div>
+              </div>
+            </div>
+            <div className="panel-section">
+              <h2 className="panel-group-label label">Filter</h2>
+              <div className="control-group">
+                <div className="control-wrapper">
+                  <Knob
+                    paramName="0%"
+                    fullAngle={260}
+                    paramMin={0}
+                    paramMax={1023}
+                    initialParam={0}
+                    color={true}
+                    onChange={(value) => console.log(value)}
+                  />
+                  <p className="control-label label">Cutoff</p>
+                </div>
+              </div>
+              <div className="control-group">
+                <div className="control-wrapper">
+                  <Knob
+                    paramName="0%"
+                    fullAngle={260}
+                    paramMin={0}
+                    paramMax={1023}
+                    initialParam={0}
+                    color={true}
+                    onChange={(value) => console.log(value)}
+                  />
+                  <p className="control-label label">Resonance</p>
                 </div>
               </div>
             </div>
