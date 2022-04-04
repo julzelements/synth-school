2c4e867ecf8a49003855c43f3933decaeecc720c
Julian Scharf <julian.scharf@tshirtventures.com.au>
2022-04-04 20:50:18 +1000

    rainbow styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,45 +6,46 @@ function App() {
   console.log(data);
   return (
     <div className="App">
-      <div className="filter">
-        <div className="panel-controls">
-          <div className="panel-section">
-            <div className="control-group">
-              <div className="control-wrapper">
-                <Knob
-                  paramName="0%"
-                  fullAngle={260}
-                  paramMin={0}
-                  paramMax={1023}
-                  initialParam={0}
-                  color={true}
-                  onChange={(value) => console.log(value)}
-                />
-                <p className="control-label label">Filter</p>
+      <div className="section-wrapper">
+        <div className="panel">
+          <header>
+            <h1 className="program-title">
+              <span>Init Program</span>
+            </h1>
+          </header>
+          <div className="panel-controls">
+            <div className="panel-section">
+              <div className="control-group">
+                <div className="control-wrapper">
+                  <Knob
+                    paramName="0%"
+                    fullAngle={260}
+                    paramMin={0}
+                    paramMax={1023}
+                    initialParam={0}
+                    color={true}
+                    onChange={(value) => console.log(value)}
+                  />
+                  <p className="control-label label">Filter</p>
+                </div>
+              </div>
+              <div className="control-group">
+                <div className="control-wrapper">
+                  <Knob
+                    paramName="0%"
+                    fullAngle={260}
+                    paramMin={0}
+                    paramMax={1023}
+                    initialParam={0}
+                    color={true}
+                    onChange={(value) => console.log(value)}
+                  />
+                  <p className="control-label label">Filter</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div style={{ width: "50px" }}></div>
-        <Knob
-          paramName="50%"
-          fullAngle={260}
-          paramMin={0}
-          paramMax={1023}
-          initialParam={512}
-          color={true}
-          onChange={(value) => console.log(value)}
-        />
-        <div style={{ width: "50px" }}></div>
-        <Knob
-          paramName="100%"
-          fullAngle={260}
-          paramMin={0}
-          paramMax={1023}
-          initialParam={1023}
-          color={true}
-          onChange={(value) => console.log(value)}
-        />
       </div>
     </div>
   );
