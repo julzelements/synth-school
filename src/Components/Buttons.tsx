527c67f076596bf75897dba9d9ac2509c38810b3
Julian Scharf <julian.scharf@tshirtventures.com.au>
2022-09-24 18:18:49 +1000

    refactoring out the api

--- src/Components/Buttons.tsx
+++ src/Components/Buttons.tsx
@@ -4,6 +4,7 @@ import CategoryButton from "./CategoryButton";
 
 // prettier-ignore
 import { fifthSaw, Delayed, LeidenLoop, ShapeShifter, VintageKit, NinetiesClassic, DeltaBass, MiniBass, SimpleBeat, VinylPulse, AcidVox, DutchClassic, MotorMouth, SoulFood, VocalChordz, AnalogDrums, ElectroKit, MrMatrix, SyntheBass, Boogie80s, FunkyAcid, PortaBass, TechNoir, DanceBass, GrittySquare, PunkFunk, VCTenVox, DeepHouse, KeycityBass, RawBass, VideoGame, Digisnd, fifthBrass, afxBass, Ardkore92, Duophony, Flute, HarmonBa, MTdigArp, PWMenvBA, Ratewobble, SyncLAM, SyncMtion, afxAcid, afxBeat, akunk, aliasBass, ascension, beatAndBass, bnsbeats1, bnsbeats2, bnsbeats3, bnsbeats4, bnsbeats5, centipede, deepBass, epicAcid, model800, phaseClks, wavetable, xocPlay, Anfem, ArcLead, ArpmeLead, BitterLead, Bosshog, BouncyBalls, BrokenArcade, Childhood, Chopchoon, CpuCycles, Dambuster, DarkPerc, DirtySub, Disemvowel, DistortedSqr, DrJuice, DualSaw, FmodSeq, Fake30SC, GhostTown, HardRun, HerdOfCrab, HoodieBass, Injection, Jackathon, JungleSub, Lfoiled, LittleGlitch, LoudSiren, LuFuki, MilkyWay, MonoBrass, OiOi, OnTheMoon, Portrythm, Pumpdriver, RobotEmpire, Robotspeak, RubberDuck, Scoooping, Squelf, StabbyBass, StomachWave, Stonecold, Syncwave, TechStab, TeeVeeSaw, TronIines, Werq,} from "../patches/exports";
+import { createNewPatch, getAllPatches } from "../api/Patches";
 
 const Buttons = ({
   selectPatch,
@@ -16,39 +17,6 @@ const Buttons = ({
     let randomPatch = Math.floor(Math.random() * patchArray.length);
     selectPatch(patchArray[randomPatch]);
   };
-
-  const handleSavePatch = async () => {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    
-    var raw = JSON.stringify({
-      "name": "bassWobble"
-    });
-    
-    var requestOptions: RequestInit = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
-    
-    fetch("http://localhost:3001/patches", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
-  };
-
-  const handleGetPatches = async () => {
-    var requestOptions: RequestInit = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("http://localhost:3001/patches", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
-  }
 
   // prettier-ignore
   const patchArray = [afxAcid, Injection, Fake30SC, TeeVeeSaw, fifthSaw, Delayed, LeidenLoop, ShapeShifter, VintageKit, NinetiesClassic, DeltaBass, MiniBass, SimpleBeat, VinylPulse, AcidVox, DutchClassic, MotorMouth, SoulFood, VocalChordz, AnalogDrums, ElectroKit, MrMatrix, SyntheBass, Boogie80s, FunkyAcid, PortaBass, TechNoir, DanceBass, GrittySquare, PunkFunk, VCTenVox, DeepHouse, KeycityBass, RawBass, VideoGame, Digisnd, fifthBrass, afxBass, Ardkore92, Duophony, Flute, HarmonBa, MTdigArp, PWMenvBA, Ratewobble, SyncLAM, SyncMtion, afxAcid, afxBeat, akunk, aliasBass, ascension, beatAndBass, bnsbeats1, bnsbeats2, bnsbeats3, bnsbeats4, bnsbeats5, centipede, deepBass, epicAcid, model800, phaseClks, wavetable, xocPlay, Anfem, ArcLead, ArpmeLead, BitterLead, Bosshog, BouncyBalls, BrokenArcade, Childhood, Chopchoon, CpuCycles, Dambuster, DarkPerc, DirtySub, Disemvowel, DistortedSqr, DrJuice, DualSaw, FmodSeq, Fake30SC, GhostTown, HardRun, HerdOfCrab, HoodieBass, Injection, Jackathon, JungleSub, Lfoiled, LittleGlitch, LoudSiren, LuFuki, MilkyWay, MonoBrass, OiOi, OnTheMoon, Portrythm, Pumpdriver, RobotEmpire, Robotspeak, RubberDuck, Scoooping, Squelf, StabbyBass, StomachWave, Stonecold, Syncwave, TechStab, TeeVeeSaw, TronIines, Werq,]
@@ -81,7 +49,7 @@ const Buttons = ({
           Random
         </Button>
         <Button
-          onClick={handleSavePatch}
+          onClick={() => createNewPatch(patchName)}
           size="xl"
           className="menu-button"
           color="blue"
@@ -89,7 +57,7 @@ const Buttons = ({
           Save Patch
         </Button>
         <Button
-          onClick={handleGetPatches}
+          onClick={getAllPatches}
           size="xl"
           className="menu-button"
           color="yellow"
