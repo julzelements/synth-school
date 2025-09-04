91c753efb06ac05cfa99c72522f16ec7b0c99937
Julian Scharf <julzelements@gmail.com>
2025-09-04 21:29:09 +1000

    config for relative paths

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 // next.config.ts
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   productionBrowserSourceMaps: true,
+
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
