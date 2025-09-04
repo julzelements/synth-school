758b6011ef14541e868e8052153dd714592de1ed
Julian Scharf <julzelements@gmail.com>
2025-09-04 22:01:19 +1000

    relative imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import MonologueController from "utils/midi";
-import blob from "assets/data.json";
-import App from "components/App";
-import { KorgProgramDump } from "types/types";
+import MonologueController from "../utils/midi";
+import blob from "../assets/data.json";
+import App from "../components/App";
+import { KorgProgramDump } from "../types/types";
 
 export default function Home() {
   const monologueController = new MonologueController();
