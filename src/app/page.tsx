27a00c50d7e0b7a2a94873dc2f10c0c42f6541ac
Julian Scharf <julzelements@gmail.com>
2025-09-05 07:07:52 +1000

    no escape path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@
 
 import MonologueController from "../utils/midi";
 import blob from "../assets/data.json";
-import App from "../components/App";
+
 import { KorgProgramDump } from "../types/types";
+import App from "components/App";
 
 export default function Home() {
   const monologueController = new MonologueController();
